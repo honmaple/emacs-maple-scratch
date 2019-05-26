1f3c187ee39179f45f9e6a6c4378e1c317e91607
honmaple <xiyang0807@gmail.com>
2019-05-27 01:14:39 +0800

    refactor code with clickable text rather than button
    default buffer is read only, use single key make it writable

--- maple-scratch.el
+++ maple-scratch.el
@@ -40,6 +40,11 @@
   :group 'maple-scratch
   :type 'boolean)
 
+(defcustom maple-scratch-empty t
+  "Whether empty buffer when make it writable."
+  :group 'maple-scratch
+  :type 'boolean)
+
 (defcustom maple-scratch-number 10
   "The number of show list."
   :group 'maple-scratch
@@ -47,9 +52,9 @@
 
 (defcustom maple-scratch-alist
   '(("Files"
-     :action (cond ((bound-and-true-p ivy-mode) 'ivy-recentf)
+     :action (cond ((bound-and-true-p counsel-mode) 'counsel-recentf)
                    ((bound-and-true-p helm-mode) 'helm-recentf)
-                   (t recentf-open-files))
+                   (t 'recentf-open-files))
      :source
      (progn (unless recentf-mode (recentf-mode)) recentf-list)
      :source-action 'find-file-existing
@@ -66,6 +71,9 @@
      (progn (unless (featurep 'bookmark) (require 'bookmark)) (bookmark-all-names))
      :source-action 'bookmark-jump
      :desc "Jump to Bookmark")
+    ("Agenda"
+     :action 'org-agenda
+     :desc "Open org Agenda")
     ("quit"
      :action 'save-buffers-kill-terminal
      :desc "Quit Emacs"))
@@ -77,64 +85,76 @@
   "Text show face."
   :group 'maple-scratch)
 
-(defmacro maple-scratch-insert (&rest body)
-  "Insert BODY with []."
-  `(progn
-     (insert (maple-scratch--text "["))
-     ,@body
-     (insert (maple-scratch--text "]\t"))))
+(defmacro maple-scratch-with (&rest body)
+  "Execute the forms in BODY with window."
+  (declare (indent 0) (debug t))
+  `(with-current-buffer (get-buffer-create maple-scratch-buffer) ,@body))
 
-(defmacro maple-scratch-dolist (spec &rest body)
-  "Like dolist but get INDEX, SPEC &REST BODY."
-  (declare (indent 1) (debug ((symbolp form &optional form) body)))
-  `(let ((num 0))
-     (dolist ,(cdr spec)
-       (let ((,(car spec) num))
-         ,@body
-         (setq num (+ num 1))))))
+(defmacro maple-scratch-concat (items &rest body)
+  "ITEMS &REST BODY."
+  (declare (indent 0) (debug t))
+  `(mapconcat
+    'identity
+    (cl-loop for index from 0 for item in ,items collect ,@body) "\n"))
 
 (defun maple-scratch--subseq (seq start end)
   "Slice SEQ with START END."
-  (let ((len (length seq)))
-    (cl-subseq seq start (and (number-or-marker-p end)
-                              (min len end)))))
+  (cl-subseq seq start (and (number-or-marker-p end) (min (length seq) end))))
 
 (defun maple-scratch--text (text &optional face)
   "TEXT add FACE."
   (propertize text 'font-lock-face (or face 'maple-scratch-face)))
 
-(defun maple-scratch--button (label action &optional face help)
-  "Button LABEL ACTION &OPTIONAL FACE HELP."
-  (insert-button (format "%s" label)
-                 'action
-                 `(lambda (_) (call-interactively (or (command-remapping ,action) ,action)))
-                 'follow-link t
-                 'face (or face 'font-lock-keyword-face)
-                 'help-echo (or help label)))
+(defun maple-scratch--keybind (action)
+  "Lookup ACTION keys dynamically."
+  (mapconcat 'key-description (where-is-internal action overriding-local-map) ", "))
 
-(defun maple-scratch--item (source action)
-  "Button SOURCE ACTION &OPTIONAL -FACE."
-  (insert "\n")
-  (maple-scratch-dolist (index item source)
-    (maple-scratch-insert
-     (maple-scratch--button index `(lambda ()(interactive) `(,action ,item))))
-    (maple-scratch--button item `(lambda ()(interactive) (,action ,item)) 'font-lock-comment-face)
-    (insert "\n")))
+(defun maple-scratch--keymap (action)
+  "Make keymap with ACTION."
+  (let ((map (make-sparse-keymap)))
+    (define-key map [mouse-1]
+      `(lambda (_)
+         (interactive "e")
+         (call-interactively (or (command-remapping ,action) ,action))))
+    (define-key map (kbd "RET")
+      `(lambda ()
+         (interactive)
+         (call-interactively (or (command-remapping ,action) ,action))))
+    map))
 
-(defun maple-scratch--init(label action &optional desc)
-  "Insert LABEL ACTION DESC."
-  (maple-scratch-insert
-   (maple-scratch--button label action))
-  (maple-scratch--button (or desc label) action 'font-lock-comment-face)
-  (insert "\n"))
+(defun maple-scratch--button (label keymap &optional face)
+  "LABEL ACTION &OPTIONAL KEYMAP FACE."
+  (propertize
+   (format "%s" label)
+   'face (or face 'font-lock-keyword-face)
+   'mouse-face 'highlight
+   ;; use button propertize to use next-button and previous-button
+   'button (cons (list t) keymap)
+   'keymap keymap))
 
-(defun maple-scratch--init-with-source(label action source source-action desc)
-  "Insert LABEL &KEY ACTION SOURCE SOURCE-ACTION DESC."
-  (maple-scratch-insert
-   (maple-scratch--button label action))
-  (if source (eval `(maple-scratch--item (maple-scratch--subseq ,source 0 maple-scratch-number) ,source-action))
-    (insert "\t" (maple-scratch--text desc)))
-  (insert "\n"))
+(defun maple-scratch-item(label action &optional source source-action desc)
+  "LABEL ACTION &OPTIONAL SOURCE SOURCE-ACTION DESC."
+  (let ((keymap (maple-scratch--keymap action)))
+    (concat
+     (maple-scratch--text "[")
+     (maple-scratch--button label keymap)
+     (maple-scratch--text "]")
+     (if source
+         (format "\n%s\n"
+                 (maple-scratch-concat
+                   (maple-scratch--subseq (eval `,source) 0 maple-scratch-number)
+                   (maple-scratch-item
+                    index `(lambda() (interactive) (funcall ,source-action ,item)) nil nil item)))
+       (format "\t%s" (maple-scratch--button (or desc label) keymap 'font-lock-comment-face))))))
+
+(defun maple-scratch-default()
+  "Default."
+  (maple-scratch-concat
+    maple-scratch-alist
+    (cl-destructuring-bind (label &key action source source-action desc) item
+      (if maple-scratch-source
+          (maple-scratch-item label action source source-action desc)
+        (maple-scratch-item index action nil nil desc)))))
 
 (defun maple-scratch-startup ()
   "Insert start message with FACE."
@@ -158,52 +178,58 @@
                          "\n$" ""    ; remove trailing linebreak
                          (shell-command-to-string
                           "fortune -a | fmt -80 -s | cowsay -$(shuf -n 1 -e b d g p s t w y) -f $(shuf -n 1 -e $(cowsay -l | tail -n +2)) -n")))))))
-           (concat prefix "Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))))
-
-(defun maple-scratch-default()
-  "Insert default message."
-  (maple-scratch-dolist (index args maple-scratch-alist)
-    (cl-destructuring-bind (label &key action source source-action desc) args
-      (if maple-scratch-source
-          (maple-scratch--init-with-source label action source source-action desc)
-        (maple-scratch--init index action desc)))))
+           (concat prefix "Happy hacking, " user-login-name " - Emacs ♥ you!\n"))))
 
 (defun maple-scratch-init()
   "Init maple-scratch."
-  (with-current-buffer (get-buffer-create maple-scratch-buffer)
+  (maple-scratch-with
     (insert (maple-scratch-fortune))
-    (maple-scratch-default)
-    (insert (maple-scratch-startup))))
+    (insert "\n")
+    (insert (maple-scratch-default))
+    (insert "\n")
+    (insert (maple-scratch-startup))
+    (read-only-mode 1)))
 
-(defun maple-scratch-previous-button ()
-  "Previous button."
+(defun maple-scratch-previous-item ()
+  "Previous item."
   (interactive)
   (move-beginning-of-line 1)
   (let ((btn (previous-button (point))))
     (if btn (goto-char (button-start btn))
       (goto-char (point-max))
-      (maple-scratch-previous-button))))
+      (maple-scratch-previous-item))))
 
-(defun maple-scratch-next-button ()
-  "Next button."
+(defun maple-scratch-next-item ()
+  "Next item."
   (interactive)
   (move-end-of-line 1)
   (let ((btn (next-button (point))))
     (if btn (goto-char (button-start btn))
       (goto-char (point-min))
-      (maple-scratch-next-button))))
+      (maple-scratch-next-item))))
+
+(defun maple-scratch-writable ()
+  "Writable."
+  (interactive)
+  (maple-scratch-with
+    (read-only-mode -1)
+    (when maple-scratch-empty (erase-buffer))
+    (assq-delete-all 'maple-scratch-mode minor-mode-map-alist)))
 
 (defvar maple-scratch-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-n") #'maple-scratch-next-button)
-    (define-key map (kbd "C-p") #'maple-scratch-previous-button)
+    (define-key map (kbd "C-n") #'maple-scratch-next-item)
+    (define-key map (kbd "C-p") #'maple-scratch-previous-item)
     (define-key map (kbd "C-q") #'save-buffers-kill-terminal)
+    (define-key map (kbd "n") #'maple-scratch-next-item)
+    (define-key map (kbd "p") #'maple-scratch-previous-item)
+    (define-key map (kbd "e") #'maple-scratch-writable)
+    (define-key map (kbd "q") #'save-buffers-kill-terminal)
     map) "Keymap of command `maple-scratch-mode'.")
 
 ;;;###autoload
 (define-minor-mode maple-scratch-mode
   "maple-scratch-mode."
-  :global nil
   :keymap maple-scratch-mode-map
   (when maple-scratch-mode (maple-scratch-init)))
 
