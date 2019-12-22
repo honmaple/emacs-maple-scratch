f0dd8877a428e3df30d1fdbc95fdb78b9a7c1463
honmaple <xiyang0807@gmail.com>
2019-12-22 17:38:26 +0800

    add banner and navbar

--- maple-scratch.el
+++ maple-scratch.el
@@ -40,6 +40,11 @@
   :group 'maple-scratch
   :type 'boolean)
 
+(defcustom maple-scratch-center t
+  "Whether make scratch center."
+  :group 'maple-scratch
+  :type 'boolean)
+
 (defcustom maple-scratch-empty t
   "Whether empty buffer when make it writable."
   :group 'maple-scratch
@@ -50,10 +55,48 @@
   :group 'maple-scratch
   :type 'number)
 
+(defcustom maple-scratch-banner
+  '("      ___           ___           ___           ___       ___"
+    "     /\\__\\         /\\  \\         /\\  \\         /\\__\\     /\\  \\"
+    "    /::|  |       /::\\  \\       /::\\  \\       /:/  /    /::\\  \\"
+    "   /:|:|  |      /:/\\:\\  \\     /:/\\:\\  \\     /:/  /    /:/\\:\\  \\"
+    "  /:/|:|__|__   /::\\~\\:\\  \\   /::\\~\\:\\  \\   /:/  /    /::\\~\\:\\  \\"
+    " /:/ |::::\\__\\ /:/\\:\\ \\:\\__\\ /:/\\:\\ \\:\\__\\ /:/__/    /:/\\:\\ \\:\\__\\"
+    " \\/__/~~/:/  / \\/__\\:\\/:/  / \\/__\\:\\/:/  / \\:\\  \\    \\:\\~\\:\\ \\/__/"
+    "       /:/  /       \\::/  /       \\::/  /   \\:\\  \\    \\:\\ \\:\\__\\"
+    "      /:/  /        /:/  /         \\/__/     \\:\\  \\    \\:\\ \\/__/"
+    "     /:/  /        /:/  /                     \\:\\__\\    \\:\\__\\"
+    "     \\/__/         \\/__/      EMACS ♥ YOU!     \\/__/     \\/__/")
+  "The banner of show list."
+  :group 'maple-scratch
+  :type 'list)
+
+;; (defcustom maple-scratch-banner
+;;   '(" _______  _______  _______  _        _______"
+;;     "(       )(  ___  )(  ____ )( \\      (  ____ \\"
+;;     "| () () || (   ) || (    )|| (      | (    \\/"
+;;     "| || || || (___) || (____)|| |      | (__"
+;;     "| |(_)| ||  ___  ||  _____)| |      |  __)"
+;;     "| |   | || (   ) || (      | |      | ("
+;;     "| )   ( || )   ( || )      | (____/\\| (____/\\"
+;;     "|/     \\||/     \\||/       (_______/(_______/")
+;;   "The banner of show list."
+;;   :group 'maple-scratch
+;;   :type 'list)
+
 (defcustom maple-scratch-write-mode 'emacs-lisp-mode
   "The default mode when writable."
   :group 'maple-scratch
   :type 'function)
+
+(defcustom maple-scratch-items '(maple-scratch-fortune
+                                 maple-scratch-banner
+                                 maple-scratch-navbar
+                                 maple-scratch-default
+                                 maple-scratch-startup)
+  "The item of show list."
+  :group 'maple-scratch
+  :type 'list)
 
 (defcustom maple-scratch-alist
   '(("Files"
@@ -86,6 +129,23 @@
   :group 'maple-scratch
   :type '(list))
 
+(defcustom maple-scratch-navbar-alist
+  '(("HOME"
+     :action (lambda() (find-file (expand-file-name "init.el" user-emacs-directory)))
+     :desc "Browse home")
+    ("RESTORE"
+     :action 'restore-session
+     :desc "Restore Session")
+    ("AGENDA"
+     :action 'org-agenda
+     :desc "Open org Agenda")
+    ("HELP"
+     :action (lambda() (find-file (expand-file-name "README.org" user-emacs-directory)))
+     :desc "Quit Emacs"))
+  "Nav list of insert."
+  :group 'maple-scratch
+  :type '(list))
+
 (defface maple-scratch-face '((t (:inherit font-lock-comment-face)))
   "Text show face."
   :group 'maple-scratch)
@@ -112,7 +172,7 @@
 
 (defun maple-scratch--keybind (action)
   "Lookup ACTION keys dynamically."
-  (mapconcat 'key-description (where-is-internal action overriding-local-map) ", "))
+  (mapcar 'key-description (where-is-internal action overriding-local-map)))
 
 (defun maple-scratch--keymap (action)
   "Make keymap with ACTION."
@@ -120,20 +180,21 @@
     (define-key map [mouse-1]
       `(lambda (_)
          (interactive "e")
-         (call-interactively (or (command-remapping ,action) ,action))))
+         (funcall (or (command-remapping ,action) ,action))))
     (define-key map (kbd "RET")
       `(lambda ()
          (interactive)
-         (call-interactively (or (command-remapping ,action) ,action))))
+         (funcall (or (command-remapping ,action) ,action))))
     map))
 
-(defun maple-scratch--button (label keymap &optional face)
-  "LABEL ACTION &OPTIONAL KEYMAP FACE."
+(defun maple-scratch--button (label keymap &optional face help)
+  "LABEL ACTION &OPTIONAL KEYMAP FACE HELP."
   (propertize
    (format "%s" label)
    'face (or face 'font-lock-keyword-face)
    'mouse-face 'highlight
    ;; use button propertize to use next-button and previous-button
+   'help-echo help
    'button (cons (list t) keymap)
    'keymap keymap))
 
@@ -149,11 +210,11 @@
                  (maple-scratch-concat
                    (maple-scratch--subseq (eval `,source) 0 maple-scratch-number)
                    (maple-scratch-item
-                    index `(lambda() (interactive) (funcall ,source-action ,item)) nil nil item)))
+                    index `(lambda() (funcall ,source-action ,item)) nil nil item)))
        (format "\t%s" (maple-scratch--button (or desc label) keymap 'font-lock-comment-face))))))
 
 (defun maple-scratch-default()
-  "Default."
+  "Insert default message in scratch buffer."
   (maple-scratch-concat
     maple-scratch-alist
     (cl-destructuring-bind (label &key action source source-action desc) item
@@ -162,38 +223,88 @@
         (maple-scratch-item index action nil nil desc)))))
 
 (defun maple-scratch-startup ()
-  "Insert start message with FACE."
+  "Insert start message in scratch buffer."
   (maple-scratch--text
-   (format "\nEmacs startup finished in %.2fms with %s packages\n"
+   (format "Emacs startup finished in %.2fms with %s packages"
            (* 1000.0 (float-time (time-subtract after-init-time before-init-time)))
            (length load-path))))
 
 (defun maple-scratch-fortune(&optional prefix)
   "Insert fortune message with PREFIX."
-  (maple-scratch--text
-   (concat (when (executable-find "fortune")
-             (format (concat prefix "%s\n\n")
-                     (replace-regexp-in-string
-                      "\\[[0-9]*m" "" ; remove chinese shell char
-                      (replace-regexp-in-string
-                       "\n" (concat "\n" prefix) ; comment each line
-                       (replace-regexp-in-string
-                        "\s*$" ""    ; remove spaces
-                        (replace-regexp-in-string
-                         "\n$" ""    ; remove trailing linebreak
-                         (shell-command-to-string
-                          "fortune -a | fmt -80 -s | cowsay -$(shuf -n 1 -e b d g p s t w y) -f $(shuf -n 1 -e $(cowsay -l | tail -n +2)) -n")))))))
-           (concat prefix "Happy hacking, " user-login-name " - Emacs ♥ you!\n"))))
+  (let ((fortune (when (executable-find "fortune")
+                   "fortune -a | fmt -80 -s"))
+        (cowsay (when (executable-find "cowsay")
+                  (cond ((executable-find "shuf")
+                         "cowsay -$(shuf -n 1 -e b d g p s t w y) -f $(shuf -n 1 -e $(cowsay -l | tail -n +2)) -n")
+                        ((executable-find "gshuf")
+                         "cowsay -$(gshuf -n 1 -e b d g p s t w y) -f $(gshuf -n 1 -e $(cowsay -l | tail -n +2)) -n")))))
+    (maple-scratch--text
+     (format (concat prefix "%s\n\n")
+             (replace-regexp-in-string
+              "\\[[0-9]*m" "" ; remove chinese shell char
+              (replace-regexp-in-string
+               "\n" (concat "\n" prefix) ; comment each line
+               (replace-regexp-in-string
+                "\s*$" ""    ; remove spaces
+                (replace-regexp-in-string
+                 "\n$" ""    ; remove trailing linebreak
+                 (cond ((and fortune cowsay)
+                        (shell-command-to-string (format "%s | %s" fortune cowsay)))
+                       (fortune
+                        (shell-command-to-string fortune))
+                       (t ""))))))))))
+
+(defun maple-scratch-banner()
+  "Insert banner message."
+  (when maple-scratch-banner
+    (maple-scratch--text
+     (mapconcat 'identity maple-scratch-banner "\n"))))
+
+(defun maple-scratch-navbar()
+  "Insert navbar message."
+  (mapconcat
+   (lambda(item)
+     (cl-destructuring-bind (label &key action desc) item
+       (concat
+        (maple-scratch--text "[")
+        (maple-scratch--button label (maple-scratch--keymap action) nil desc)
+        (maple-scratch--text "]"))))
+   maple-scratch-navbar-alist " "))
 
 (defun maple-scratch-init()
   "Init maple-scratch."
   (maple-scratch-with
-    (insert (maple-scratch-fortune))
-    (insert "\n")
-    (insert (maple-scratch-default))
-    (insert "\n")
-    (insert (maple-scratch-startup))
-    (read-only-mode 1)))
+    (insert (mapconcat
+             (lambda(x)
+               (if maple-scratch-center
+                   (maple-scratch--center (funcall x))
+                 (funcall x)))
+             maple-scratch-items "\n\n"))
+    (read-only-mode 1)
+    (when maple-scratch-center
+      (add-hook 'window-size-change-functions #'maple-scratch--resize nil t))))
+
+(defun maple-scratch--resize(&rest _)
+  "Resize buffer content, Make it center."
+  (interactive)
+  (maple-scratch-with
+    (let ((inhibit-read-only t)
+          (content (buffer-string)))
+      (erase-buffer)
+      (insert (maple-scratch--center content)))))
+
+(defun maple-scratch--center(content)
+  "Make CONTENT center of buffer."
+  (let ((max-len 0)
+        (content (with-temp-buffer
+                   (insert content)
+                   (indent-rigidly (point-min) (point-max) (- (indent-rigidly--current-indentation (point-min) (point-max))))
+                   (buffer-string))))
+    (cl-loop for line in (split-string content "\n")
+             do (let ((len (length line))) (when (> len max-len) (setq max-len len))))
+    (mapconcat
+     (lambda(s) (concat (make-string (ceiling (max 0 (- (window-width) max-len)) 2) ? ) s))
+     (split-string content "\n") "\n")))
 
 (defun maple-scratch-previous-item ()
   "Previous item."
@@ -217,9 +328,8 @@
   "Writable."
   (interactive)
   (maple-scratch-with
-    (read-only-mode -1)
+    (maple-scratch-mode -1)
     (when maple-scratch-empty (erase-buffer))
-    (assq-delete-all 'maple-scratch-mode minor-mode-map-alist)
     (when maple-scratch-write-mode (funcall maple-scratch-write-mode))))
 
 (defvar maple-scratch-mode-map
@@ -237,7 +347,9 @@
 (define-minor-mode maple-scratch-mode
   "maple-scratch-mode."
   :keymap maple-scratch-mode-map
-  (when maple-scratch-mode (maple-scratch-init)))
+  (if maple-scratch-mode (maple-scratch-init)
+    (read-only-mode -1)
+    (remove-hook 'window-size-change-functions #'maple-scratch--resize)))
 
 (provide 'maple-scratch)
 ;;; maple-scratch.el ends here
